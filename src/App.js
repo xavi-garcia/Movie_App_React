477e69169967793e936428190b62c2599973c5ad
Javier Garcia <trad.ljgarcia@gmail.com>
2022-09-25 01:24:38 -0300

    creted new app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from 'react';
+import Movie from './Movie'
+
+
 
 function App() {
+  const [popular, setPopular] = useState([]);
+  
+  useEffect(()=> {
+    fetchPopular();
+  }, []);
+
+  const fetchPopular = async () => {
+      const data = await fetch(
+        'https://api.themoviedb.org/3/movie/popular?api_key=2da23600bef7520c85495217292df2ae&language=en-US&page=1'
+      );
+      const movies = await data.json();
+      setPopular(movies.results);
+  };
+
   return (
+    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <div className='popular-movies'>
+        {popular.map((movie) => {
+        return <Movie key={movie.id}/>
+      })}
+      </div>
     </div>
   );
 }
---
+++ src/Movie.js
@@ -0,0 +1,10 @@
+function Movie(){
+    return (
+        <div>
+            <h2>Title</h2>
+            <img src="" alt="" />
+        </div>
+    );
+}
+
+export default Movie;
